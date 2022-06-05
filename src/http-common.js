d1a90122fa44666d9087562d9f7229d3cc627333
Owais Ali <owaisali@ricult.com>
2022-06-06 01:58:23 +0500

    connect with deployed server

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://todo-node-pg-server.herokuapp.com/",
+  // baseURL: "http://localhost:5000/",
   headers: {
     "Content-type": "application/json"
   }
